6d033ac36d305664dbd7f106a6a5b4ff3c980bf5
Taranjeet Singh <reachtotj@gmail.com>
2018-05-25 18:48:06 +0530

    store: add book model

--- djlibrary/store/models.py
+++ djlibrary/store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+
+    name = models.CharField(max_length=255)
+    isbn_number = models.CharField(max_length=13)
+
+    class Meta:
+        db_table = 'book'
+
+    def __str__(self):
+        return self.name
