036ab48211e28cbf37dcb91e5a3d82192a1d95a2
Taranjeet Singh <reachtotj@gmail.com>
2019-05-07 18:07:02 +0530

    add home view.

--- djlibrary/djlibrary/urls.py
+++ djlibrary/djlibrary/urls.py
@@ -20,6 +20,6 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     path('store/', include('store.urls', namespace='store') ),
 ]
